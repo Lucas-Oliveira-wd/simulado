14bc8b4f506ddd3d15574ed4a13b950ccd6ca17f
Lucas Oliveira <lucasoliveira5978@gmail.com>
2025-08-12 20:15:13 -0300

    :up: commit antes de formatar

--- sim.R
+++ sim.R
@@ -2,16 +2,16 @@
 #############            CRIAR SIMULADO DE QUESTÕES            #################
 ################################################################################
 
-lista_de_pdfs = list(portugues = 0:13,
+lista_de_pdfs = list(portugues = 0:12,
                       ingles = 0:7,
-                      con_esp = 0:18,
+                      con_esp = c(0:21,31:33),
                       cont_custos = 0:6,
-                      cont_ger = 0:12,
-                      estatistica = 0:18,
-                      mat_fin = 0:7)
+                      cont_ger = 0:5,
+                      financas = 0:1,
+                      mat_fin_estat = 0:10)
 
 criar_sim = function(size_por, size_ing, size_cesp, size_ccus, size_cger,
-                     size_est, size_mfin) {
+                     size_financ, size_mfin_est) {
   print("questões de português:")
   print(sort(sample(lista_de_pdfs$portugues, size_por, replace = T)))
   print("questões de ingles:")
@@ -22,16 +22,16 @@ criar_sim = function(size_por, size_ing, size_cesp, size_ccus, size_cger,
   print(sort(sample(lista_de_pdfs$cont_custos, size_ccus, replace = T)))
   print("questões de contabilidade gerencial:")
   print(sort(sample(lista_de_pdfs$cont_ger, size_cger, replace = T)))
-  print("questões de estatística:")
-  print(sort(sample(lista_de_pdfs$estatistica, size_est, replace = T)))
-  print("questões de matemárica financeira:")
-  print(sort(sample(lista_de_pdfs$mat_fin, size_mfin, replace = T)))
+  print("questões de finanças:")
+  print(sort(sample(lista_de_pdfs$financas, size_financ, replace = T)))
+  print("questões de matemárica financeira e estatística:")
+  print(sort(sample(lista_de_pdfs$mat_fin_estat, size_mfin_est, replace = T)))
 }
 
 criar_sim(size_por = 15,
           size_ing = 5,
-          size_cesp = 50,
+          size_cesp = 30,
           size_ccus = 5,
           size_cger = 5,
-          size_est = 5,
-          size_mfin = 5)
+          size_financ = 5,
+          size_mfin_est = 5)
